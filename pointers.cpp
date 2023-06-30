bf4a8ca2487c5a14f6c8267b9ecd3ab4206ae401
Caio Brainer <92734524+CaioBrainer@users.noreply.github.com>
2023-06-29 23:44:55 -0300

    Add files via upload

--- pointers.cpp
+++ pointers.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+//Protótipos de funções
+void double_pointer(int *pointer);
+//-----------------------------------------------------------------------------------------
+
 int main() {
 	SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8); //funciona para output com acentuação.
@@ -28,18 +32,18 @@ int main() {
  * identificador ou ao tipo do ponteiro.
  **/
 	int *ponteiro = nullptr;
-	cout << "\nAddress of pointer: " << &ponteiro <<endl;
-	cout << "Value of pointer: " << ponteiro <<endl;
-	cout << "Size of pointer: " << sizeof(ponteiro) <<endl;
+	cout << "\nEndereço do ponteiro: " << &ponteiro <<endl;
+	cout << "Valor do ponteiro: " << ponteiro <<endl;
+	cout << "tamanho do ponteiro: " << sizeof(ponteiro) <<endl;
  	cout << "-----------------------------------------------------------------------------" <<endl;
 	
 /* Acessando um valor alocado no endereço para o qual o ponteiro
  * aponta
  * */
 	int *number_pointer = &number;
-	cout << "\nAddress inside the number pointer: " << number_pointer <<endl;
+	cout << "\nEndereço de memória no ponteiro: " << number_pointer <<endl;
 	//* para "derreferenciar" um pointer
-	cout << "Value inside the number variable: " << *number_pointer <<endl;
+	cout << "Valor alocado no endereço de memória para onde o ponteiro aponta: " << *number_pointer <<endl;
  	cout << "-----------------------------------------------------------------------------" <<endl;
 	
 /* Iterando em vetores através de pointers:
@@ -80,8 +84,6 @@ int main() {
 	cout << "\n\n" << "pointer_s1 == pointer_s2? " <<(pointer_s1 == pointer_s2);
 	cout << "\n" << "*pointer_s1 == *pointer_s2? " <<(*pointer_s1 == *pointer_s2) << endl;
 	
-	
-
 /* Arrays e Pointers
  * */
 	int numeros[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -91,11 +93,34 @@ int main() {
 	cout << "\nPara acessar valores em arrays e vetores também podemos usar *((array ou pointer) + 5)" <<endl;
 	cout << "O resultado é o mesmo: " <<*(numeros+5) << " e " << *(numeros_ptr+5) << endl;
 
-/*
+/* Pointers e Constants.
+ * Podemos associar constante ao valor associado ao endereço de 
+ * memória para o qual o ponteiro está direcionado ou definir o 
+ * endereço como constante. Os valor e endereço podem ser 
+ * constantes de forma simultânea.
  * */
-	
  	cout << "\n-----------------------------------------------------------------------------" <<endl;
+	double pi {3.1415};
+	const double *pi_ptr {&pi}; //Aqui o valor associado ao endereço será constante
+	cout << *pi_ptr << " Nesse caso não poderá ser alterado" <<endl; 
+	cout << pi_ptr << " Nesse caso poderá ser alterado" <<endl;
+	
+	double const *pi_ptr2 {&pi}; //Nesse caso o endereço será constante
+	cout << "\n" << *pi_ptr2 <<" Nesse caso poderá ser alterado" <<endl;
+	cout << pi_ptr2 << " Nesse caso não poderá ser alterado" <<endl;
 
+/* Pointers e Functions.
+ * Passagem de valores ou refeências de ponteiros como argumentos de função. Podemos passar um 
+ * ponteiro ou passar o endereço de uma variável com o operador de endereço &.
+ * */
+ 	cout << "\n-----------------------------------------------------------------------------" <<endl;
+ 	cout << "Passando uma pointeiro de inteiro para uma variável e multiplicando por 2: " <<endl;
+	double_pointer(number_pointer);
 	
 	return 0;
 }
+
+// Declaração de funções
+void double_pointer(int *pointer) {
+	cout << (*pointer * 2);
+		}
